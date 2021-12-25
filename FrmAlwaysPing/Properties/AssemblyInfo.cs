c1f363ba9bd2d6165867f6310c2fb58b03142bcc
ryder1992 <matteobern92@hotmail.com>
2021-12-25 11:53:22 +0100

    change name app

--- FrmAlwaysPing/Properties/AssemblyInfo.cs
+++ FrmAlwaysPing/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("FrmAlwaysPing")]
+[assembly: AssemblyTitle("AlwaysPing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FrmAlwaysPing")]
+[assembly: AssemblyProduct("AlwaysPing")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
